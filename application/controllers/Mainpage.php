4bc5ee0a5d1c9b415c7cda1b0994d5975f93460f
John Mark Sena <30517958+srphfthnd@users.noreply.github.com>
2017-10-10 14:48:47 +0800

    Update Mainpage.php

--- application/controllers/Mainpage.php
+++ application/controllers/Mainpage.php
@@ -7,6 +7,8 @@ class Mainpage extends CI_Controller {
 		parent::__construct();
 		$this->load->helper('url');
 		$this->load->model('users_model','user');
+		$this->load->model('recipes_model','recipe');
+		//$this->load->library('sessions');
 	}
 	
 	
@@ -31,7 +33,12 @@ class Mainpage extends CI_Controller {
 	}
 	public function Login()
 	{
-		$data['title'] = "ROTW: Login";					
+		$enter =  $this->session->userdata('username');
+		if( !empty($enter) ){
+			 redirect (base_url('Mainpage/Admin/'));
+		}	
+		
+		$data['title'] = "Login";			
 		$this->load->view('include/header',$data);
 		$this->load->view('mainpage/login');
 	}
@@ -44,12 +51,37 @@ class Mainpage extends CI_Controller {
 	}	
 	public function Admin()
 	{
-		$data['title'] = "ROTW: Admin";					
+		$enter =  $this->session->userdata('username');
+		if( empty($enter) ){
+			 redirect (base_url('Mainpage/Login/'));
+		}				
+		$data['title'] = "Welcome Admin!";
+		
 		$this->load->view('include/header',$data);
 		$this->load->view('mainpage/dashboard');
+	}
+
+	public function AdminLogin()
+	{
+		$data['title'] = "ROTW: Admin Login" ;					
+		$this->load->view('include/header',$data);
+		$this->load->view('mainpage/login_admin');
 	}	
-	
+	public function Category()
+	{
+		$data['title'] = "ROTW Admin: Category" ;					
+		$this->load->view('include/header',$data);
+		$this->load->view('mainpage/admin/categories');
+	}	
+		public function ARecipe()
+	{
+		$data['title'] = "ROTW Admin: Recipe" ;					
+		$this->load->view('include/header',$data);
+		$this->load->view('mainpage/admin/recipe_admin');
+	}	
 	public function signupAction(){
+
+
 				
 		$username = $this->input->post('username');
 		$email = $this->input->post('email');
@@ -57,12 +89,17 @@ class Mainpage extends CI_Controller {
 		$surname = $this->input->post('surname');
 		$firstname = $this->input->post('firstname');
 		$miname = $this->input->post('miname');
-		$birthmonth = $this->input->post('birthmonth');
+		// $birthmonth = $this->input->post('birthmonth');
 		$birthdate = $this->input->post('birthdate');
-		$birthyear = $this->input->post('birthyear');
+		// $birthyear = $this->input->post('birthyear');
 		$address = $this->input->post('address');
+
+		/* $this->form_validation->set_rules('username', 'Username', 'trim|required|min_length[5]|max_length[16]');
+		$this->form_validation->set_rules('password', 'Password', 'trim|required|min_length[8]');
+		$this->form_validation->set_rules('cpassword', 'Password Confirmation', 'trim|required|matches[password]');
+		$this->form_validation->set_rules('email', 'Email', 'trim|required|valid_email'); */
 		
-		$flag = $this->user->signup($username,$email,$password,$surname,$firstname,$miname,$birthmonth,$birthdate,$birthyear,$address);
+		$flag = $this->user->signup($username,$email,$password,$surname,$firstname,$miname,$birthdate,$address);
 		if($flag){
 			
 			redirect(base_url('Mainpage/Home/'));
@@ -74,25 +111,63 @@ class Mainpage extends CI_Controller {
 	
 	//Login Comparing
 	public function loginAction() {
-		
-		$username = $this->input->post('username');
-		$password = $this->input->post('password');
-		
-		//users_model verify
-		$row = $this->user->login($username, $password);
-		if(count($row)>1){
-		
-			redirect(base_url('Mainpage/Admin'));
-		// print_r($row);
-
-		}
-		else{
-		//If invalid Email or Password			
-			echo 'NANII?? Incorrect Username or Password';
-			
-		}
-		
+		$this->session->set_userdata($this->user->Login($this->input->post('username'), $this->input->post('password')));
 	}	
+			
+	public function adminloginAction() {
+		$this->session->set_userdata($this->user->Adminlogin($this->input->post('username'), $this->input->post('password')));
+	}	
+	
+	 public function logout() {
+		$this->session->sess_destroy();
+		redirect(base_url('Mainpage/Home/'));
+		
+	}
+
+	public function insertRecipe() {
+		
+		$enter =  $this->session->userdata('username');
+		if( empty($enter) ){
+			 redirect (base_url('Mainpage/Login/'));
+		}	
+		$this->load->view('Mainpage/admin/insert_recipe');
+	}
+	
+	public function submitRecipe() {
+		
+		$recipename = $this->input->post('recipename');
+		$recipetype = $this->input->post('recipetype');
+		$recipedetails = $this->input->post('recipedetails');
+		$recipeimg = $this->input->post('recipeimg');
+		$recipecost = $this->input->post('recipecost');
+		$recipeprocedure = $this->input->post('recipeprocedure');
+		
+		$flag = $this->recipe->submit($recipename, $recipetype, $recipedetails, $recipeimg, $recipecost, $recipeprocedure);
+		
+		 if($flag){
+		echo 'Recipe Save!';
+		redirect(base_url('Mainpage/insertRecipe/'));
+
+					
+		}
+		else {
+			echo 'Error!!!';
+		} 
+		
+	}
+	
+	public function listRecipe(){
+		$enter =  $this->session->userdata('username');
+		if( empty($enter) ){
+			 redirect (base_url('Mainpage/Login/'));
+		}		
+		
+		$data['recipes'] = $this->recipe->recipelist('recipes');	
+		$this->load->view('Mainpage/Admin/list_recipe',$data);
+		
+	}
+	
+
 }
 
 ?>
