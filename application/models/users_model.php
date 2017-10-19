980060c0f6e96e4b629eab37ad77f67894d6f1cd
Renzo Feliciano Payod <31798160+renzofaker@users.noreply.github.com>
2017-10-20 01:24:01 +0800

    Update users_model.php

--- application/models/users_model.php
+++ application/models/users_model.php
@@ -3,61 +3,120 @@
 class Users_model extends CI_Model {
 	
 	
-	public function signup($username,$email,$password,$surname,$firstname,$miname,$birthdate,$address) {		
-		$sql = 'INSERT INTO users VALUES ("'.$username.'","'.$email.'","'.$password.'","'.$surname.'","'.$firstname.'","'.$miname.'","'.$birthdate.'","'.$address.'")';
+	public function signup($email,$email,$password,$balance,$surname,$firstname,$miname,$birthdate,$address,$usercred) {		
+		$sql = 'INSERT INTO users VALUES ("'.$email.'","'.$email.'","'.$password.'","'.$balance.'","'.$surname.'","'.$firstname.'","'.$miname.'","'.$birthdate.'","'.$address.'","'.$usercred.'")';
 		//echo $sql;
 		return $this->db->simple_query($sql);
 	}
 	
-	public function login($username, $password) {
+	public function login($email, $password) {
 		
-		$sql = 'SELECT * FROM users WHERE username = "'.$username.'" and password = "'.$password.'"';
+		$sql = 'SELECT * FROM users WHERE email = "'.$email.'" and password = "'.$password.'"';
 		$query = $this->db->query($sql);
 		$ok = false;
 		$userdata = [];
 		foreach($query->result() as $row){
 			$userdata = array(
-				'username' => $row->username,
+				'email' => $row->email,
 				'logged_in' => true
 			);
 			$ok = true;
 		}
-		if($ok){			
-			$this->session->set_userdata('username', $_POST['username']);
-			// echo $username;
-			redirect(base_url('Mainpage/Admin/'));
-			
-		}else{
-			echo "Invalid username or password";
+		if($ok){		
+			$this->session->set_userdata('email', $_POST['email'],'usercred',$_POST['usercred']);
+			// echo $email;
+			redirect(base_url('Mainpage/User/'));
+		}
+		else{
+			echo "Invalid email or password";
 		}
 	}
 	
-	public function adminlogin($username, $password) {
+	public function userdat($usercred){
 		
-		$sql = 'SELECT * FROM users WHERE username = "'.$username.'" and password = "'.$password.'"';
+		$name = $this->session->userdata('email');
+		$sql = "SELECT usercred FROM users WHERE email = '$name'";
+		$query = $this->db->query($sql);
+		$ok = false;
+		$newdata = [];
+		foreach($query->result() as $row){
+			$userdata = array(
+				'usercred' => $row->usercred
+			);
+			$ok = true;
+		}
+		if($ok){
+		$this->session->set_userdata($newdata);
+		
+		print_r($newdata);
+		}
+		else{
+			echo 'Error!';
+		}
+	}
+	
+	public function adminlogin($email, $password, $usercred) {
+		
+		$sql = 'SELECT * FROM users WHERE email = "'.$email.'" and password = "'.$password.'"';
 		$query = $this->db->query($sql);
 		$ok = false;
 		$userdata = [];
 		foreach($query->result() as $row){
 			$userdata = array(
-				'username' => $row->username,
+				'email' => $row->email,
+				'usercred' => $row->usercred,
 				'logged_in' => true
 			);
+			$re='usercred';
 			$ok = true;
+			
 		}
 		if($ok){			
-			$this->session->set_userdata('username', $_POST['username']);
-			// echo $username;
+			$this->session->set_userdata('email', $_POST['email']);
+			// echo $email;
 			redirect(base_url('Mainpage/Admin/'));
-			
-		}else{
-			echo "Invalid username or password";
+		}
+		else{
+			echo "Invalid email or password";
 		}
 	}
+	
+	public function authority($name){
+		// $report = '';
+		// $name = ;
+		$query = $this->db->query("SELECT usercred FROM users WHERE email = '$name'");
 		
-	public function changeUsername($username){
+		foreach($query->result_array() as $row){
+			return $row['usercred'];
+		}	
+		// return $report;
+	}
+	
+	public function authority1($firstname){
+		// $report = '';
+		// $name = ;
+		$query = $this->db->query("SELECT firstname FROM users WHERE email = '$firstname'");
+		
+		foreach($query->result_array() as $row){
+			return $row['firstname'];
+		}	
+		// return $report;
+	}
 
-		$sql = "UPDATE users SET username = '$username' WHERE user_id = {$userId}";
+	public function bal($balance){
+		$report = [];
+		$name = $this->session->userdata('email');
+		$query = $this->db->query("SELECT balance FROM users WHERE email = '$name'");
+		
+		foreach($query->result_array() as $row){
+			$report[] = $row;
+		}
+		return $report;
+	}
+		
+	public function changeemail($email){
+
+		$sql = "UPDATE users SET email = '$email' WHERE user_id = {$userId}";
 		
 	}
 	
@@ -82,8 +141,8 @@ class Users_model extends CI_Model {
 		return TRUE;	
 	}
 	
-	public function del($username){
-		$query = $this->db->query("DELETE FROM users where username = '".$username."'");
+	public function del($email){
+		$query = $this->db->query("DELETE FROM users where email = '".$email."'");
 	}
 	
 }
