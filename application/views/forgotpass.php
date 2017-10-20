a3b3caeadc8af6f645c3799a002f0e2227d5de59
vincezoleta <31659155+vincezoleta@users.noreply.github.com>
2017-10-20 15:37:07 +0800

    Add files via upload

--- application/views/forgotpass.php
+++ application/views/forgotpass.php
@@ -1,6 +1,18 @@
-<html>
+<script>
 
-<body>
+    $(function() {
+    $('#email,#password').on('keypress', function(e) {
+        if (e.which == 32)
+            return false;
+    });
+});
+</script>
+
+<!-- Preloader -->
+<div id="preloader">
+    <div id="load"></div>
+  </div>
+
 <section id="intro" class="intro" style="padding-top: 140px;">
 <div class="container">
     <div class="row vertical">
@@ -8,16 +20,39 @@
       <div class="col-md-6 col-md-offset-3" style="padding-left: 60px;">
         <div class="panel panel-info">
           <div class="panel-heading" style="background-color: #351E24;">
-<form id="intro" class="intro" action="/action_page.php">
-  New password:<br>
-  <input type="password" name="new password">
-  <br>
-  Confirm password:<br>
-  <input type="password" name="confirm password">
-  <br><br>
-  <input type="submit" value="Submit">
-</form> 
-</div></div></div></div></div>
+ 
+            <h3 class="panel-title" style="color: white;">Enter your Email</h3>
+          </div>
+          <div class="panel-body">
 
-</body>
-</html>
+            <form class="form-horizontal" action="<?php echo base_url('Mainpage'); ?>" method="post" id="loginForm">
+              <fieldset style="padding-top: 10px;">
+             
+             <div class="form-group" style="padding-left: 30px;">
+                  <label for="password" class="col-xs-1 control-label" style="color: #351E24;">Email: </label>
+                  <div class="col-xs-10" style="padding-left: 72px; padding-right: 17px;">
+                    <input type="Email" required minlength="8" maxlength="40" class="form-control" id="password" name="password" placeholder="Enter email" autocomplete="off" /> 
+                    <fieldset style="padding-top: 10px;">
+					<h6>Your password will redirect to inbox</h6>
+					</div>
+                </div>
+
+                <div class="form-group" style="padding-left: 50px; padding-top: 0px;">
+                  <div class="col-md-offset-3 col-sm-5">
+                    <button type="submit" class="btn btn-default" style="background-color: #351E24; color: white;"> <i class="glyphicon glyphicon-log-in"></i> Send</a></button>
+                </div>
+                  </div>          
+              </fieldset>
+            </form>
+          </div>
+          <!-- panel-body -->
+        </div>
+        <!-- /panel -->
+      </div>
+      <!-- /col-md-4 -->
+    </div>
+    <!-- /row -->
+  </div>
+  <!-- container -->  
+</div>
+</section>
